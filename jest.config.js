0afa39edd78576b758bfa254e991e8b8aae30d6a
dehanz13 <hansel.danniel@gmail.com>
2022-01-29 12:47:02 -0600

    add: coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,12 @@ module.exports = {
     },
   },
   setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
